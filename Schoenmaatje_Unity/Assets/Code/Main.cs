b9e1790a44335db55b7ee6dadcc0942eb5652c29
DESKTOP-D969JQD\Tobias <tobiasvdven@hotmail.com>
2018-01-15 22:04:46 +0100

    Initial commit, empty Unity project for Schoenmaatje, with just a Main.cs script containing a Debug.Log() example.

---
+++ Schoenmaatje_Unity/Assets/Code/Main.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+	void Start()
+    {
+        Debug.Log("Hoi Derek.");
+        Debug.Log("Debug.Log() is een handige functie om dingen te kunnen printen in de Unity log, zie hier voor meer C# tutorials: https://unity3d.com/learn/tutorials/s/scripting.");
+        Debug.Log("Focus ligt daar op game development, maar ook wel wat C# in het algemeen.");
+	}
+}
